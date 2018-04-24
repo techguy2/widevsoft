ad126fc8161c36a0f9bdf7fe455fdea312bf5034
Soeb <Soeb@Soeb-PC>
2018-04-24 11:14:26 -0500

    Test Commit

--- src/main/java/com/widevsoft/rest/controller/UserController.java
+++ src/main/java/com/widevsoft/rest/controller/UserController.java
@@ -11,6 +11,6 @@ public class UserController {
 
 	@RequestMapping(value="/users/{id}", method= RequestMethod.POST)	
 	public String userPackageInfo(@PathVariable("id") int userId){	
-		return "Hello";
+		return "Helldfo";
 	}
 }
